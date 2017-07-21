37ed035a8f8f40cb8e643efbe9153be3f04044c1
elenoGH <goeleno@gmail.com>
2017-07-21 11:53:27 -0500

    agregar nueva tabbla con nuevos estulos y scripts

--- js/custom.js
+++ js/custom.js
@@ -1,28 +1,207 @@
 /* Codigo JavaScript a la medida */
 $(document).ready(function() {
-    $('#tabla-datos-abiertos').DataTable({
-    language: {
-        processing:     "Traitement en cours...",
-        search:         "Buscar :",
-        lengthMenu:    "Mostra _MENU_ ",
-        info:           "Resultados _START_ a _END_ de _TOTAL_ elementos",
-        infoEmpty:      "Resultados 0 &agrave; 0 a 0 elementos",
-        infoFiltered:   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
-        infoPostFix:    "",
-        loadingRecords: "Chargement en cours...",
-        zeroRecords:    "Aucun &eacute;l&eacute;ment &agrave; afficher",
-        emptyTable:     "Aucune donnée disponible dans le tableau",
-        paginate: {
-            first:      "Primero",
-            previous:   "Anterior",
-            next:       "Siguiente",
-            last:       "Último"
-        },
-        aria: {
-            sortAscending:  ": activer pour trier la colonne par ordre croissant",
-            sortDescending: ": activer pour trier la colonne par ordre décroissant"
-        }
+//    $('#tabla-datos-abiertos').DataTable({
+//    language: {
+//        processing:     "Traitement en cours...",
+//        search:         "Buscar :",
+//        lengthMenu:    "Mostra _MENU_ ",
+//        info:           "Resultados _START_ a _END_ de _TOTAL_ elementos",
+//        infoEmpty:      "Resultados 0 &agrave; 0 a 0 elementos",
+//        infoFiltered:   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+//        infoPostFix:    "",
+//        loadingRecords: "Chargement en cours...",
+//        zeroRecords:    "Aucun &eacute;l&eacute;ment &agrave; afficher",
+//        emptyTable:     "Aucune donnée disponible dans le tableau",
+//        paginate: {
+//            first:      "Primero",
+//            previous:   "Anterior",
+//            next:       "Siguiente",
+//            last:       "Último"
+//        },
+//        aria: {
+//            sortAscending:  ": activer pour trier la colonne par ordre croissant",
+//            sortDescending: ": activer pour trier la colonne par ordre décroissant"
+//        }
+//    }
+//});
+//    $.fn.DataTable.FixedColumns
+
+var responsiveHelper = undefined;
+    var breakpointDefinition = {
+        tablet: 1024,
+        phone: 480
+    };
+
+    // Initialize datatable showing a search box at the top right corner
+    var initTableWithSearch = function() {
+        var table = $('#tableWithSearch');
+
+        var settings = {
+            "sDom": "<t><'row'<p i>>",
+            "destroy": true,
+            "scrollCollapse": true,
+            "oLanguage": {
+                "sLengthMenu": "_MENU_ ",
+                "sInfo": "Showing <b>_START_ to _END_</b> of _TOTAL_ entries"
+            },
+            "iDisplayLength": 5
+        };
+
+        table.dataTable(settings);
+
+        // search box for table
+        $('#search-table').keyup(function() {
+            table.fnFilter($(this).val());
+        });
     }
+
+    // Initialize datatable with ability to add rows dynamically
+    var initTableWithDynamicRows = function() {
+        var table = $('#tableWithDynamicRows');
+
+
+        var settings = {
+            "sDom": "<t><'row'<p i>>",
+            "destroy": true,
+            "scrollCollapse": true,
+            "oLanguage": {
+                "sLengthMenu": "_MENU_ ",
+                "sInfo": "Showing <b>_START_ to _END_</b> of _TOTAL_ entries"
+            },
+            "iDisplayLength": 5
+        };
+
+
+        table.dataTable(settings);
+
+        $('#show-modal').click(function() {
+            $('#addNewAppModal').modal('show');
+        });
+
+        $('#add-app').click(function() {
+            table.dataTable().fnAddData([
+                $("#appName").val(),
+                $("#appDescription").val(),
+                $("#appPrice").val(),
+                $("#appNotes").val()
+            ]);
+            $('#addNewAppModal').modal('hide');
+
+        });
+    }
+
+    // Initialize datatable showing export options
+    var initTableWithExportOptions = function() {
+        var table = $('#tableWithExportOptions');
+
+
+        var settings = {
+            "sDom": "<'exportOptions'T><'table-responsive't><'row'<p i>>",
+            "destroy": true,
+            "scrollCollapse": true,
+            "oLanguage": {
+                "sLengthMenu": "_MENU_ ",
+                "sInfo": "Showing <b>_START_ to _END_</b> of _TOTAL_ entries"
+            },
+            "iDisplayLength": 5,
+            "oTableTools": {
+                "sSwfPath": "assets/plugins/jquery-datatable/extensions/TableTools/swf/copy_csv_xls_pdf.swf",
+                "aButtons": [{
+                    "sExtends": "csv",
+                    "sButtonText": "<i class='pg-grid'></i>",
+                }, {
+                    "sExtends": "xls",
+                    "sButtonText": "<i class='fa fa-file-excel-o'></i>",
+                }, {
+                    "sExtends": "pdf",
+                    "sButtonText": "<i class='fa fa-file-pdf-o'></i>",
+                }, {
+                    "sExtends": "copy",
+                    "sButtonText": "<i class='fa fa-copy'></i>",
+                }]
+            },
+            fnDrawCallback: function(oSettings) {
+                $('.export-options-container').append($('.exportOptions'));
+
+                $('#ToolTables_tableWithExportOptions_0').tooltip({
+                    title: 'Export as CSV',
+                    container: 'body'
+                });
+
+                $('#ToolTables_tableWithExportOptions_1').tooltip({
+                    title: 'Export as Excel',
+                    container: 'body'
+                });
+
+                $('#ToolTables_tableWithExportOptions_2').tooltip({
+                    title: 'Export as PDF',
+                    container: 'body'
+                });
+
+                $('#ToolTables_tableWithExportOptions_3').tooltip({
+                    title: 'Copy data',
+                    container: 'body'
+                });
+            }
+        };
+
+
+        table.dataTable(settings);
+
+    }
+
+    initTableWithSearch();
+    initTableWithDynamicRows();
+    initTableWithExportOptions();
+    
+        $(".list-view-wrapper").scrollbar();
+
+        $('[data-pages="search"]').search({
+            // Bind elements that are included inside search overlay
+            searchField: '#overlay-search',
+            closeButton: '.overlay-close',
+            suggestions: '#overlay-suggestions',
+            brand: '.brand',
+             // Callback that will be run when you hit ENTER button on search box
+            onSearchSubmit: function(searchString) {
+                console.log("Search for: " + searchString);
+            },
+            // Callback that will be run whenever you enter a key into search box. 
+            // Perform any live search here.  
+            onKeyEnter: function(searchString) {
+                console.log("Live search for: " + searchString);
+                var searchField = $('#overlay-search');
+                var searchResults = $('.search-results');
+
+                /* 
+                    Do AJAX call here to get search results
+                    and update DOM and use the following block 
+                    'searchResults.find('.result-name').each(function() {...}'
+                    inside the AJAX callback to update the DOM
+                */
+
+                // Timeout is used for DEMO purpose only to simulate an AJAX call
+                clearTimeout($.data(this, 'timer'));
+                searchResults.fadeOut("fast"); // hide previously returned results until server returns new results
+                var wait = setTimeout(function() {
+
+                    searchResults.find('.result-name').each(function() {
+                        if (searchField.val().length != 0) {
+                            $(this).html(searchField.val());
+                            searchResults.fadeIn("fast"); // reveal updated results
+                        }
+                    });
+                }, 500);
+                $(this).data('timer', wait);
+
+            }
+        })
+        
+        $('.panel-collapse label').on('click', function(e){
+            e.stopPropagation();
+        })
 });
-    $.fn.DataTable.FixedColumns
-});
+
+    
+    
+    
